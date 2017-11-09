87233eef8068977b4066e5463f4c160fad345432
Brian Gunn <github@gunnbr.org>
2017-11-08 21:42:52 -0800

    Implemented factoid search

--- server.js
+++ server.js
@@ -13,6 +13,15 @@ app.get('/', (req, res) => {
     res.send('hello world')
 })
 
+app.get('/factoids/:search', (req, res) => {
+    console.log('Factoid search for ' + req.params.search)
+    
+    auth.db.query("SELECT item, value, nick FROM factoids WHERE item LIKE ?", [req.params.search], function (err, result, fields) {
+        if (err) throw err;
+
+        res.send(result)
+    })
+})
 
 app.use('/auth', auth.router)
 app.listen(3000);
